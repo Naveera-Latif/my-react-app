ce415c8527fe0f9141fa896b0f808957d8b51aa4
Naveera-Latif <66240901+Naveera-Latif@users.noreply.github.com>
2020-06-04 00:57:44 +0500

    change in src/app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World fron naveera.thanks.</h1>
+      <h1>Hello World fron naveera.thanks you </h1>
     </div>
   );
 }
